f162028cb36dbe67a3356772baa9d80f2336f617
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-10-14 15:25:49 +0200

    -

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,33 +20,15 @@
           <br>
           <div class="row" id="skillDiv">
             <h3>Skills:</h3>
-                <div class="col-sm-6 lg-12" id="skill"> 
+                <div class="col-sm-6 col-12" id="skill"> 
                     <h4>Coding Skills</h4>
-                    <!-- <div v-for="skill in skillsData" :key="skill.name" >
-                      <h5>
-                        <span>></span> {{ skill.name }}
-                      </h5>   
-                    </div>      -->
-                    <h5>
-                        <span>></span>HTML <i class="lab la-html5"></i> 
-                      </h5>  
-                      <h5>
-                        <span>></span>CSS <i class="lab la-css3"></i>
-                      </h5>                
-                      <h5>
-                        <span>></span>BOOTSTRAP <i class="lab la-bootstrap"></i> 
-                      </h5>                
-                      <h5>
-                        <span>></span>VUE.JS <i class="lab la-vuejs"></i>
-                      </h5>                
-                      <h5>
-                        <span>></span>JAVASCRIPT <i class="lab la-js"></i> 
-                      </h5>                
-                      <h5>
-                        <span>></span>NODE.JS <i class="lab la-node-js"></i> 
-                      </h5>                
+                    <div v-for="skill in skillsData" :key="skill.name" >
+                        <h5>
+                            <span>></span> {{ skill.name }}
+                        </h5>  
+                    </div>
                 </div>
-                <div class="col-sm-6 lg-12" id="skill">
+                <div class="col-sm-6 col-12" id="skill">
                     <h4>Soft Skills</h4>
                     <h5>
                         <span>></span> Problem-Solving and Critical Thinking
@@ -118,7 +100,7 @@ export default {
     margin:auto;
     padding:10px;
     background-color:rgb(204, 192, 255);
-    width:100%;
+    width:90%;
     border:none;
     text-shadow: 2px 2px 4px rgb(163, 163, 163);
   }
@@ -127,21 +109,17 @@ export default {
     font-size: 20px;
   }
   #skill{
-    text-align: start;
+    text-align: center;
     background-color: #bb7aad;
     border-radius: 10px;
-    /* width:35%; */
-    /* margin-left: 130px; */
-    margin: auto;
+    width:35%;
+    margin-left: 130px;
   }
   #skillDiv{
-    width: 100%;
     margin-top: 30px;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    margin: auto;
-    padding-left: 30px;
   }
   .cursor{
     font-size:30px;
@@ -168,7 +146,6 @@ export default {
     #skill{
       text-align: center;
       margin-top: 20px;
-      font-size: 18px;
     }
   }
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -24,10 +24,13 @@
           <p v-if="project.showDescription">{{ project.description }}</p>
 
           <div id="bnts" class="align-items-center">
-            <button @click="toggleDescription(index)" class="btn btn-outline-custom">
+            <button @click="toggleDescription(index)" class="btn btn-outline-custom" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="project description">
                 <i class="bi bi-info-circle-fill"></i>
             </button>
-            <a :href="project.gitHub" target="_blank" class="btn btn-outline-custom mx-1"><i class="bi bi-github"></i></a>
+            <a :href="project.gitHub" target="_blank" class="btn btn-outline-custom mx-1"
+            data-bs-toggle="tooltip" data-bs-placement="top"
+              data-bs-custom-class="custom-tooltip"
+              data-bs-title="project description"><i class="bi bi-github"></i></a>
             <a :href="project.vercel" target="_blank" class="btn btn-outline-custom mx-1"><i class="bi bi-rocket-takeoff-fill"></i></a>
           </div>
         </div>
@@ -36,8 +39,8 @@
       
   </template>
   <script>
-  
   // import SpinnerComp from '/src/components/SpinnerComp.vue'
+  // import 'bootstrap';
   export default {
       // components:{
       //   SpinnerComp
@@ -99,8 +102,12 @@
         this.blinkCursor();
         this.filterProjects.forEach((_, index) => {
           this.startTypingEffect(index);
-          this.projectVisibility[index] = false;
+          this.projectVisibility[index] = false; // Initialize visibility state
         })
+        // const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        //   tooltipTriggerList.forEach(tooltipTriggerEl => {
+        //     new bootstrap.Tooltip(tooltipTriggerEl);
+        //   });
       }
     };
   </script>
@@ -133,9 +140,10 @@
       animation: blink-caret 0.75s step-end infinite;
     }
     img{
-      width:100%;
+      width:95%;
       height:250px;
       border-radius: 10px;
+      /* box-shadow: 10px 10px 5px #670652; */
       box-shadow: 0 15px 25px #670652;
       margin-bottom: 20px;
       transition: transform 0.5s ease, box-shadow 0.5s ease;
@@ -165,7 +173,7 @@
     .card{
       background-color: rgba(128, 0, 128, 0.0);
       width:40%;
-      min-height: 350px;
+      height: 650px;
       margin-left:70px;
       margin-bottom:30px;
       border: none;
@@ -178,6 +186,7 @@
       justify-content: center; 
       align-items: center;
       margin-top: 20px; 
+
       margin: auto;
     }
 
